71cc4fe35b21ffbacbfddfc270d1300da21d4a0c
Gokhan <0xgokhan@pm.me>
2023-07-04 06:54:09 +0300

    initial commit

---
+++ src/screens/MacbookPro/MacbookPro.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="div">
+        <img
+          className="conceptual-artist"
+          alt="Conceptual artist"
+          src="/img/conceptual-artist-author-indie-researcher-experimental-poetics.png"
+        />
+        <h1 className="text-wrapper">
+          proactive artist experimenting at the longhouse of finance, art, compute to appreciate the use of knowledge in
+          society.
+        </h1>
+        <div className="TWITTER-FARCASTER">
+          <a href="https://twitter.com/0xgokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              TWITTER
+              <br />
+            </span>
+          </a>
+          <a href="https://fcast.me/gokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              FARCASTER
+              <br />
+            </span>
+          </a>
+          <a href="https://bsky.app/profile/0xgokhan.bsky.social" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              BLUESKY
+              <br />
+            </span>
+          </a>
+          <a href="https://gokhan.substack.com" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              SUBSTACK
+              <br />
+            </span>
+          </a>
+          <a href="https://gokhan.mirror.xyz" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              MIRROR
+              <br />
+            </span>
+          </a>
+          <a href="https://paragraph.xyz/@gokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              PARAGRAPH
+              <br />
+            </span>
+          </a>
+          <a href="https://medium.com/@0xgokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              MEDIUM
+              <br />
+            </span>
+          </a>
+          <a href="https://github.com/gigabvgatti" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              GITHUB
+              <br />
+            </span>
+          </a>
+          <a href="https://t.me/gigabvgatti" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              TELEGRAM
+              <br />
+            </span>
+          </a>
+          <a href="https://discord.gg/QQQEbXAJWE" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              DISCORD
+              <br />
+            </span>
+          </a>
+          <a href="https://linktr.ee/0xgokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              LINKTREE
+              <br />
+            </span>
+          </a>
+          <a href="https://lu.ma/gokhan" rel="noopener noreferrer" target="_blank">
+            <span className="span">
+              LUMA
+              <br />
+            </span>
+          </a>
+        </div>
+        <p className="conceptual-art">
+          conceptual art
+          <br />
+          curation
+          <br />
+          blockspace
+          <br />
+          finance in art
+          <br />
+          competitive governance
+        </p>
+      </div>
+    </div>
+  );
+};
